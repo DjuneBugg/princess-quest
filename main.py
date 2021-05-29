eada3504b50523cbb035adc976c853f0f8a8983b
DjuneBugg <junipersandeman@gmail.com>
2021-05-29 10:28:04 +1000

    Game now displays images

--- main.py
+++ main.py
@@ -1,126 +1,60 @@
+# import the pygame module, so you can use it
+import pygame
 
-def game(true=None):
-    lockpick = False
-    sword = False
-    pinky = True
-    tower = False
-    dungeon = False
-    fight = False
-    sneak = False
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
-    print('A princess has been captured by a dragon. The king has offered a castle as a reward for rescuing her.')
-    valid_text = False
-    while not valid_text:
-        txt = input('Choose your equipment for the adventure (lockpick or sword): ')
-        if txt.lower() == 'lockpick':
-            lockpick = True
-            valid_text = True
-        elif txt.lower() == 'sword':
-            sword = True
-            valid_text = True
-        else:
-            print('You must enter "lockpick" or "sword"')
+# define a main function
+def main():
+    # initialize the pygame module
+    pygame.init()
 
-    valid_text = False
-    while not valid_text:
-        txt = input("You approach the dragon's castle, a goblin guards the entrance.\n"
-                    "Do you fight the goblin or sneak past? (fight or sneak): ")
-        if txt.lower() == 'fight':
-            fight = True
-            valid_text = True
-        elif txt.lower() == 'sneak':
-            sneak = True
-            valid_text = True
-        else:
-            print('You must enter "fight" or "sneak"')
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((680, 480))
 
-    if fight:
-        if sword:
-            print('You defeat the goblin with your sword')
-        else:
-            print('You have no sword! While fighting, the goblin manages to nibble your pinky off')
-            pinky = False
+    # Initialise text
+    pygame.font.init()  # you have to call this at the start,
+    # if you want to use this module.
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render('Welcome to Princess Quest!', False, (0, 0, 0))
 
-    if sneak:
-        if sword:
-            print('The goblin catches a glimpse of your big shiny sword! While fighting, the goblin manages to nibble your pinky off')
-            pinky = False
-        else:
-            print('You manage to sneak past the goblin')
+    # define a variable to control the main loop
+    running = True
 
-    valid_text = False
-    while not valid_text:
-        txt = input("You enter the castle, do you think the princess is in the tower or the dungeon? (tower or dungeon): ")
-
-        if txt.lower() == 'tower':
-            tower = True
-            valid_text = True
-        elif txt.lower() == 'dungeon':
-            dungeon = True
-            valid_text = True
-        else:
-            print('You must enter "tower" or "dungeon"')
-
-    print('Hooray! You found the princess door, but there is a code')
-    if lockpick:
-        print('Thankfully you got through the door with the lockpick!')
-
-    else:
-        valid_text = False
-        tries = 0
-        while not valid_text:
-            txt = input(
-                "6 x Pineapple = 24\nPineapple x Apple = 32\nWhat does apple = ")
-            tries = tries + 1
-
-            if txt == '8':
-                valid_text = True
-            else:
-                if tries < 3:
-                    print('Incorrect! Try again')
-                elif tries < 10:
-                    print('Hint: Pineapple = 4')
-                else:
-                    print('You give up and break the door with your sword. Your sword breaks')
-                    valid_text = True
-                    sword = False
-    print("You enter the room and it turns out that the dragon is guarding the princess himself!")
-    if sword:
-        print('The dragon breathes fire and melts your sword!')
-        sword = False
-
-    valid_text = False
-    while not valid_text:
-        txt = input('The dragon whips his tail at you. The tail is 5 meters long and you are 3 meters away.'
-                    'How far back do you walk (in meters)?: ')
-        number = float(txt)
-        if number > 3:
-            print('You stepped too far back and fell onto the stairs. Try again')
-        elif number > 2:
-            print('You dodged it, great job!')
-            valid_text = True
-        elif number == 2:
-            print('Wow, that was close, but you dodged it, great job!')
-            valid_text = True
-        else:
-            print('Oh no! You got smacked to the ground!')
-    print('The dragon was about to step on you when you sneak under him!')
-    txt = input('You get to the princess and she says "thank you for saving me" with a sparkle in her eye\nDo you wish to return her for the castle or run away and fall in love with her (run away or return): ')
-    if txt == 'run away':
-        print('You get married to the princess, but she complains that you do not have a castle')
-    else:
-        print('You take the princess to the king and he gives you a castle, but it is VERY lonely')
-    if not pinky:
-        print('AND you do not have a pinky')
-
-        print(f"lockpick={lockpick}")
-        print(f'sword={sword}')
-        print(f'pinky={pinky}')
-        print(f'tower={tower}')
-        print(f'dungeon={dungeon}')
+    background1 = Background('pictures/dragon_castle.jpg', [0, 0])
+    background2 = Background('pictures/goblin.jpg', [0, 0])
+    background = background1
+    screen.blit(background.image, background.rect)
+    screen.blit(textsurface, (0, 0))
+    pygame.display.flip()
 
 
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+            # Did the user hit a key?
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if background == background1:
+                        background = background2
+                    else:
+                        background = background1
+                # Add text
+                screen.fill((0, 0, 0))
+                screen.blit(background.image, background.rect)
+                screen.blit(textsurface, (0, 0))
+                pygame.display.flip()
 
 # Main entry point for script
 if __name__ == '__main__':
-    game()
+    main()
---
+++ text_version.py
@@ -0,0 +1,122 @@
+
+def game(true=None):
+    lockpick = False
+    sword = False
+    pinky = True
+    tower = False
+    dungeon = False
+    fight = False
+    sneak = False
+
+    print('A princess has been captured by a dragon. The king has offered a castle as a reward for rescuing her.')
+    valid_text = False
+    while not valid_text:
+        txt = input('Choose your equipment for the adventure (lockpick or sword): ')
+        if txt.lower() == 'lockpick':
+            lockpick = True
+            valid_text = True
+        elif txt.lower() == 'sword':
+            sword = True
+            valid_text = True
+        else:
+            print('You must enter "lockpick" or "sword"')
+
+    valid_text = False
+    while not valid_text:
+        txt = input("You approach the dragon's castle, a goblin guards the entrance.\n"
+                    "Do you fight the goblin or sneak past? (fight or sneak): ")
+        if txt.lower() == 'fight':
+            fight = True
+            valid_text = True
+        elif txt.lower() == 'sneak':
+            sneak = True
+            valid_text = True
+        else:
+            print('You must enter "fight" or "sneak"')
+
+    if fight:
+        if sword:
+            print('You defeat the goblin with your sword')
+        else:
+            print('You have no sword! While fighting, the goblin manages to nibble your pinky off')
+            pinky = False
+
+    if sneak:
+        if sword:
+            print('The goblin catches a glimpse of your big shiny sword! While fighting, the goblin manages to nibble your pinky off')
+            pinky = False
+        else:
+            print('You manage to sneak past the goblin')
+
+    valid_text = False
+    while not valid_text:
+        txt = input("You enter the castle, do you think the princess is in the tower or the dungeon? (tower or dungeon): ")
+
+        if txt.lower() == 'tower':
+            tower = True
+            valid_text = True
+        elif txt.lower() == 'dungeon':
+            dungeon = True
+            valid_text = True
+        else:
+            print('You must enter "tower" or "dungeon"')
+
+    print('Hooray! You found the princess door, but there is a code')
+    if lockpick:
+        print('Thankfully you got through the door with the lockpick!')
+
+    else:
+        valid_text = False
+        tries = 0
+        while not valid_text:
+            txt = input(
+                "6 x Pineapple = 24\nPineapple x Apple = 32\nWhat does apple = ")
+            tries = tries + 1
+
+            if txt == '8':
+                valid_text = True
+            else:
+                if tries < 3:
+                    print('Incorrect! Try again')
+                elif tries < 10:
+                    print('Hint: Pineapple = 4')
+                else:
+                    print('You give up and break the door with your sword. Your sword breaks')
+                    valid_text = True
+                    sword = False
+    print("You enter the room and it turns out that the dragon is guarding the princess himself!")
+    if sword:
+        print('The dragon breathes fire and melts your sword!')
+        sword = False
+
+    valid_text = False
+    while not valid_text:
+        txt = input('The dragon whips his tail at you. The tail is 5 meters long and you are 3 meters away.'
+                    'How far back do you walk (in meters)?: ')
+        number = float(txt)
+        if number > 3:
+            print('You stepped too far back and fell onto the stairs. Try again')
+        elif number > 2:
+            print('You dodged it, great job!')
+            valid_text = True
+        elif number == 2:
+            print('Wow, that was close, but you dodged it, great job!')
+            valid_text = True
+        else:
+            print('Oh no! You got smacked to the ground!')
+    print('The dragon was about to step on you when you sneak under him!')
+    txt = input('You get to the princess and she says "thank you for saving me" with a sparkle in her eye\nDo you wish to return her for the castle or run away and fall in love with her (run away or return): ')
+    if txt == 'run away':
+        print('You get married to the princess, but she complains that you do not have a castle')
+    else:
+        print('You take the princess to the king and he gives you a castle, but it is VERY lonely')
+    if not pinky:
+        print('AND you do not have a pinky')
+
+        print(f"lockpick={lockpick}")
+        print(f'sword={sword}')
+        print(f'pinky={pinky}')
+        print(f'tower={tower}')
+        print(f'dungeon={dungeon}')
+
+
